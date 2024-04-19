90abda47ad3df54d9436fa3a8b465bee332808b0
simonebute <bedrockresources21@gmail.com>
2024-04-19 04:35:03 -0700

    task0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# Creates a file
-file { 'tmp/school':
+# Creates a filie
+file { '/tmp/school':
 	mode	=> '0744',
 	owner	=> 'www-data',
 	group	=> 'www-data',
-	content	=> 'i love Puppet'
+	content	=> 'I love Puppet'
 }
